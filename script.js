8fb0e6fe2f10e3af4c16671769684345e8126ac8
Adam <adamdepollo@gmail.com>
2020-01-09 20:15:58 -0500

    added jQuery to control drop down menus in writing page, standardized Bootstrap swatch on all pages

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-var $me = $("#me");
-var $offset = (($("body").css("width") - $me.css("height"))/2) + "vh";
-$me.css("margin-left", $offset);
+let $homeBack = $("#homeBack");
+let $offset = (($("body").css("width") - $homeBack.css("height"))/2) + "vh";
+$homeBack.css("left", $offset);
 let $homeImage = $("#homeImage");
-var $homeDivs = $("#homeImage .fade-in");
+let $homeDivs = $("#homeImage .fade-in");
 $homeImage.mouseover(function(){
-  $me.css("opacity", "0%");
+  $homeBack.css("opacity", "0%");
     $homeDivs.css({
-      "transition": "1s",
+      "transition": "2s",
       "opacity": "100%"
     });
 });
-function oop(){
-  console.log("oop")
-}
 
-oop();
+let $writingHeader = $(".writingHeader");
+$writingHeader.hover(function(){
+  $('html,body').css('cursor','pointer');
+  $(this).children().children("h4").addClass("writingHover");
+}, function(){
+  $('html,body').css('cursor','default');
+  $(this).children().children("h4").removeClass("writingHover");
+});
+
+$writingHeader.click(function(){
+  $(this).children().children(".writingChild").toggle("slow");
+});
