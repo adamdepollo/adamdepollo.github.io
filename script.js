03fc1ff6afe499f55e9b3d116b75449e614c7982
Adam <adamdepollo@gmail.com>
2020-01-09 23:19:30 -0500

    deleted unnecessary external script references

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let $homeBack = $("#homeBack");
-let $offset = (($("body").css("width") - $homeBack.css("height"))/2) + "vh";
+let $offset = (($("body").css("width") - $homeBack.css("height")) / 2) + "vh";
 $homeBack.css("left", $offset);
 let $homeImage = $("#homeImage");
 let $homeDivs = $("#homeImage .fade-in");
-$homeImage.mouseover(function(){
+$homeImage.mouseover(function() {
   $homeBack.css("opacity", "0%");
-    $homeDivs.css({
-      "transition": "2s",
-      "opacity": "100%"
-    });
+  $homeDivs.css({
+    "transition": "2s",
+    "opacity": "100%"
+  });
 });
 
 let $writingHeader = $(".writingHeader");
-$writingHeader.hover(function(){
-  $('html,body').css('cursor','pointer');
+$writingHeader.hover(function() {
+  $('html,body').css('cursor', 'pointer');
   $(this).children().children("h4").addClass("writingHover");
-}, function(){
-  $('html,body').css('cursor','default');
+}, function() {
+  $('html,body').css('cursor', 'default');
   $(this).children().children("h4").removeClass("writingHover");
 });
 
-$writingHeader.click(function(){
+$writingHeader.click(function() {
   $(this).children().children(".writingChild").toggle("slow");
 });
